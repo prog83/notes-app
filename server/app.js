c7dce9337bbe8a55ac19ed961e82acb6e22accf9
o.myronov.1607@in.np.gov.ua <o.myronov.1607@in.np.gov.ua>
2019-01-15 18:46:09 +0200

    get

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 import express from "express";
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send(`Hello world!`);
+});
+
 const server = app.listen(8080, () => {
   console.log(`Server is up and running on port 8080`);
 });

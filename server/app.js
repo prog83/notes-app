199bb642fcd754c8c6840670064470dcc5b32381
o.myronov.1607@in.np.gov.ua <o.myronov.1607@in.np.gov.ua>
2019-01-16 19:18:01 +0200

    db

--- server/app.js
+++ server/app.js
@@ -1,11 +1,26 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { serverPort } from "../config";
+import * as db from "./db";
+
+db.setUpConnection();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send(`Hello world!`);
+app.use(bodyParser.json());
+
+app.get('/notes', (req, res) => {
+  db.listNotes().then(data => res.status(200).send(data));
+});
+
+app.post('/notes', (req, res) => {
+  db.createNote(req.body).then(data => res.status(200).send(data));
+});
+
+app.delete('/notes/:id', (req, res) => {
+  db.deleteNote(req.params.id).then(data => res.status(200).send(data));
 });
 
 const server = app.listen(8080, () => {
-  console.log(`Server is up and running on port 8080`);
+  console.log(`Server is up and running on port ${serverPort}`);
 });
